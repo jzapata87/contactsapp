f4ce5d03d92d10dc930032ace56b05412e791e1c
Javier Zapata <zapataj87@gmail.com>
2018-07-24 00:21:53 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+
+import contacts from './contacts'
+
+export default class App extends React.Component {
+  state = {
+    showContacts: false,
+  }
+
+  toggleContacts = () => {
+    this.setState(prevState => ({showContacts: !prevState.showContacts}))
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <View> { contacts.map(contact => (
+          <View>{contacts.name}</View>
+        ))}</View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    },
+});
